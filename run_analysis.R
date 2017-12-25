9c3d4874c287c88bc024b7cf6d5ddf10c3e8662f
Jim Harris <jim@eunomicnetworks.com>
2017-12-25 16:39:49 -0500

    rearrange into more logical order

--- run_analysis.R
+++ run_analysis.R
@@ -10,22 +10,6 @@ library(data.table)
 xtest<-read.table('UCI HAR Dataset/test/X_test.txt', stringsAsFactors = FALSE)
 xtrain<-read.table('UCI HAR Dataset/train/X_train.txt', stringsAsFactors = FALSE)
 
-# get the activity list from each, set the column name to "activity"
-activitytest<-read.table('UCI HAR Dataset/test/y_test.txt', stringsAsFactors = FALSE)
-colnames(activitytest)<-'activity'
-activitytrain<-read.table('UCI HAR Dataset/train/y_train.txt', stringsAsFactors = FALSE)
-colnames(activitytrain)<-'activity'
-
-# get the labels provided for activities, and set column names appropriately
-alabels<-read.table('UCI HAR Dataset/activity_labels.txt', stringsAsFactors = FALSE)
-colnames(alabels)<-c('activity','activityname')
-
-#get the subject numbers from each set, and set the names 
-subjectnumbertest<-read.table('UCI HAR Dataset/test/subject_test.txt', stringsAsFactors = FALSE)
-colnames(subjectnumbertest)<-'subjectnumber'
-subjectnumbertrain<-read.table('UCI HAR Dataset/train/subject_train.txt', stringsAsFactors = FALSE)
-colnames(subjectnumbertrain)<-'subjectnumber'
-
 # make a table of the data "features," and convert to a lower case vector of names
 featurenames<-read.table('UCI HAR Dataset/features.txt', stringsAsFactors = FALSE)
 featurenames<-featurenames$V2
@@ -45,9 +29,18 @@ featurenames<-gsub('bodybody','body', featurenames) # apparent typo in the origi
 colnames(xtest)<-colnames(xtrain)<-featurenames
 # # 4 satisfied - "Appropriately labels the data set with descriptive variable names."
 
-# use dplyr to gt all of the "mean","meanfreq", and "std" rownames by removing the "angle" ,
-# then grep the remaining list with "mean" or "std" - unlist them into a vector, and store in "nms" 
-nms<-featurenames %>% lapply(function(x){x[which(!grepl('^angle',x))]}) %>% lapply(function(x){x[which(grepl('mean',x) | grepl('standarddeviation',x))]}) %>% unlist
+# get the activity list from each, set the column name to "activity"
+activitytest<-read.table('UCI HAR Dataset/test/y_test.txt', stringsAsFactors = FALSE)
+colnames(activitytest)<-'activity'
+activitytrain<-read.table('UCI HAR Dataset/train/y_train.txt', stringsAsFactors = FALSE)
+colnames(activitytrain)<-'activity'
+
+#get the subject numbers from each set, and set the names 
+subjectnumbertest<-read.table('UCI HAR Dataset/test/subject_test.txt', stringsAsFactors = FALSE)
+colnames(subjectnumbertest)<-'subjectnumber'
+subjectnumbertrain<-read.table('UCI HAR Dataset/train/subject_train.txt', stringsAsFactors = FALSE)
+colnames(subjectnumbertrain)<-'subjectnumber'
+
 # cbind subject and activity to measurements
 df1<-cbind(subjectnumbertrain, activitytrain, xtrain)
 df2<-cbind(subjectnumbertest, activitytest, xtest)
@@ -56,9 +49,17 @@ df2<-cbind(subjectnumbertest, activitytest, xtest)
 df<-rbind(df1,df2)
 # #1 satisfied - Merges the training and the test sets to create one data set.
 
+# get the labels provided for activities, and set column names appropriately
+alabels<-read.table('UCI HAR Dataset/activity_labels.txt', stringsAsFactors = FALSE)
+colnames(alabels)<-c('activity','activityname')
+
 # merge with activity frame to put names to activities
 df<-merge(alabels, df, by='activity')
 # #3 now satisfied - Uses descriptive activity names to name the activities in the data set
+
+# use dplyr to gt all of the "mean","meanfreq", and "std" rownames by removing the "angle" ,
+# then grep the remaining list with "mean" or "std" - unlist them into a vector, and store in "nms" 
+nms<-featurenames %>% lapply(function(x){x[which(!grepl('^angle',x))]}) %>% lapply(function(x){x[which(grepl('mean',x) | grepl('standarddeviation',x))]}) %>% unlist
 
 # start pairing down to only the means and std data by 
 nms<-c('subjectnumber', 'activityname', nms)
